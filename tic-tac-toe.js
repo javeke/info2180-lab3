7696b4c98561784c626749cb237afd778705f849
Javier <javier.bryan@speurgroup.com>
2020-10-09 11:58:24 -0400

    adds an X or O when clicked and stores the state

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,12 +1,36 @@
 window.onload = event =>{
-    setUp();
+    gamePlay();
 }
 
-function setUp(){
+function gamePlay(){
     let board = document.getElementById('board');
     let squares = board.getElementsByTagName('div');
+    let state = new Array(9); 
+    let last = 'X';
+    let index = 0;
 
     for (let square of squares){
         square.classList.add('square');
+        square.id = index ++; 
+        
+        square.onclick = (event)=>{
+
+            if (state[event.target.id]){
+                return;
+            }
+
+            if (last === 'X'){
+                event.target.innerText = 'O';
+                event.target.classList.add('O');
+                state[event.target.id] = 'O';
+                last = 'O';
+            }
+            else if (last === 'O'){
+                event.target.innerText = 'X';
+                event.target.classList.add('X');
+                state[event.target.id] = 'X';
+                last = 'X';
+            }
+        };
     }
 }
